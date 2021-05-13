b0c7c8315f3c3c557d778bdd34a36e748f9c6196
Ana Ventura <anaventura1811@gmail.com>
2021-05-13 13:40:25 -0300

    Estilização do summary

--- src/components/Summary/SumStyle.ts
+++ src/components/Summary/SumStyle.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const SumStyle = styled.div`
- 
+ display: grid;
+ grid-template-columns: repeat(3, 1fr); /* Para repetir as caixas de mesmo tamanho num grid*/
+ gap: 2rem;
+ margin-top: -10rem; /* Margin top negativa para colocar as divs mais acima, pegando um pouco do header*/
 `;
--- src/components/Summary/Sumary.tsx
+++ src/components/Summary/Sumary.tsx
@@ -1,4 +1,6 @@
 import entradas from '../../assets/Entradas.svg';
+import saidas from '../../assets/Saidas.svg';
+import total from '../../assets/Total.svg';
 
 import { SumStyle } from './SumStyle';
 
@@ -11,6 +13,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={ entradas } alt="entradas" />
         </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={ saidas } alt="saídas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={ total } alt="total" />
+        </header>
+        <strong>R$500,00</strong>
       </div>
     </SumStyle>
   )
