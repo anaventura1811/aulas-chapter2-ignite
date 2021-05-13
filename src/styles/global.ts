3a609adf15f7103fae32cbb2253f991e842b8c36
Ana Ventura <anaventura1811@gmail.com>
2021-05-13 12:22:46 -0300

    Completa as variáveis de cores do projeto

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle `
   :root {
+    --red: #e52e4d;
+    --blue: #5429cc;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
     --background: #f0f2f5;
+    --shape: #fff;
   }
   
   * {
@@ -26,6 +35,15 @@ export const GlobalStyle = createGlobalStyle `
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased; /* Para fontes ficarem mais nítidas */
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 
 
