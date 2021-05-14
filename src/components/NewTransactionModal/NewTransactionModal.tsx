57c0ce403448981fd1d136b068da872af8b5e433
Ana Ventura <anaventura1811@gmail.com>
2021-05-14 00:24:03 -0300

    Configura mudança de cor de botões selecionados do modal

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import entrada from '../../assets/Entradas.svg';
+import saidas from '../../assets/Saidas.svg';
 import closeImg from '../../assets/close.svg';
-import { ModalStyle } from './ModalStyles';
+import { ModalStyle, TransactionTypeContainer, Button } from './ModalStyles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -9,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 const NewTransactionModal = ({isOpen, onRequestClose }: NewTransactionModalProps) => {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -26,9 +30,42 @@ const NewTransactionModal = ({isOpen, onRequestClose }: NewTransactionModalProps
 
       <ModalStyle>
         <h2>Cadastrar transação</h2>
-        <input type="text" placeholder='Título' />
-        <input type="number" placeholder='Valor' />
-        <input type="text" placeholder='Categoria' />
+        <input
+          type="text"
+          placeholder='Título'
+        />
+        <input
+          type="number"
+          placeholder='Valor'
+        />
+
+        <TransactionTypeContainer>
+          <Button
+           type="button"
+          //  className={type === 'deposit' ? 'active' : ''}
+           onClick={() => { setType('deposit')}}
+           isActive={type === 'deposit'}
+           activeColor="green"
+          >
+            <img src={entrada} alt="Entradas" />
+            <span>Entrada</span>
+          </Button>
+
+          <Button
+           type="button"
+           onClick={() => { setType('withdraw')}}
+           isActive={type === 'withdraw'}
+           activeColor="red"
+          >
+            <img src={saidas} alt="Saídas" />
+            <span>Saída</span>
+          </Button>
+        </TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder='Categoria'
+        />
         <button type="submit">
           Cadastrar
         </button>
